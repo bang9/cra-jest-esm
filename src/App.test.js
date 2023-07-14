b06aeaf85175b6f5bbdcd5c829912becaa964d20
bang9 <gusrn1423@naver.com>
2023-07-14 19:23:27 +0900

    add moduleNameMapper to map sub-path exports

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import { Swiper, SwiperSlide } from 'swiper/swiper-react.mjs';
 
-test('renders learn react link', () => {
+// swiper/react 로 import 하고 싶다면
+// 1. https://github.com/jestjs/jest/issues/9771 최신 버전의 제스트를 사용하거나
+// 2. 그게 안되면, moduleNameMapper 를 사용
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+test('renders swiper', () => {
    render(<Swiper><SwiperSlide>Slide 1</SwiperSlide></Swiper>);
    const slide = screen.getByText('Slide 1')
    expect(slide).toBeInTheDocument();
 });
+
+
